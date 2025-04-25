088403db6001d6f7841c7f5fe762b1dc3adef3c5
Ruidiaz Anabella <ana.ruidiaz@hotmail.com>
2025-04-24 21:15:49 -0300

    Add files via upload

---
+++ codigo.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+	char nombre;
+	int edad;
+	float altura;
+	
+	printf("Ingrese su nombre: ");
+	scanf ("%c", &nombre);
+	
+	printf("Ingrese su edad; \n");
+	scanf("%d", &edad);
+	
+	printf("\nIngrese su altura en metros: \n" );
+	scanf("%f", &altura);
+	
+	printf("Hola %c. Tienes %d años y mides %f.", nombre, edad, altura);
+	
+	return 0;
+}
